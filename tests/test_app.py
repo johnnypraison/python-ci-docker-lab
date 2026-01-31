f54c19a7dd0e5670dc2dd61e1dbfce7d5fdbaf53
Mr. Johnny Praison E <urk23cs8011@KARUNYA.EDU>
2026-01-31 16:20:35 +0530

    init lab

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Adds the project root directory to the python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import add
 
 def test_add():
